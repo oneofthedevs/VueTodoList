121e7ead9564407588c642cca0a816b767f7b11c
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-09-05 14:05:38 +0530

    Changes in form

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAgHWW1FuylAigl_Sj7K1pfiFy7gRm2rTE",
+//   authDomain: "vuejs-todos-dc.firebaseapp.com",
+//   databaseURL: "https://vuejs-todos-dc.firebaseio.com",
+//   projectId: "vuejs-todos-dc",
+//   storageBucket: "vuejs-todos-dc.appspot.com",
+//   messagingSenderId: "57552574197",
+//   appId: "1:57552574197:web:14df393d386d6f23fb17bc",
+//   measurementId: "G-KJW16GB8R8"
+// };
 new Vue({
   router,
   store,
